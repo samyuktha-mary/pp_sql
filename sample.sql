8268b5685f31bb79401913ab42822aed78a40dad
Sam <133033730+samyuktha-mary@users.noreply.github.com>
2025-09-17 08:44:54 +0530

    Update sample.sql

--- sample.sql
+++ sample.sql
@@ -1,1 +1,9 @@
 
+-- Declare start and end timestamps
+DECLARE utc_start_datetime TIMESTAMP DEFAULT TIMESTAMP(parser_timestamp('%Y%m%d%H%M%S', @start_run_id, 'America/Los_Angeles'));
+DECLARE utc_end_datetime   TIMESTAMP DEFAULT TIMESTAMP(parser_timestamp('%Y%m%d%H%M%S', @end_run_id, 'America/Los_Angeles'));
+
+-- Select query using declared variables
+SELECT 
+    utc_start_datetime AS start_time_utc,
+    utc_end_datetime   AS end_time_utc;
